533010634ca22ddb756b27c3a376ebb7749b51d6
지민재 <alswo471@naver.com>
2023-04-24 15:51:26 +0900

    [2023.04.24] : 엔티티 클래스 개발2
    - 인프런 강의 [실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발] 정리 및 실습

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -9,6 +9,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
@@ -16,7 +18,7 @@ public class Category {
 
     @Id
     @GeneratedValue
-    @Column(name ="category_id")
+    @Column(name = "category_id")
     private Long id;
 
     private String name;
@@ -27,10 +29,16 @@ public class Category {
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Item> items = new ArrayList<>();
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
